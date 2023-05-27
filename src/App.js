edeab128d1ef0c31920b770a2694f796aa94f871
Mantikor1 <jonas.toens@kabelsurfer.de>
2023-05-27 10:24:43 +0200

    added registration html/css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login/Login";
 import Registration from "./Pages/Login/Registration";
 import LoginLayout from "./Pages/Login/LoginLayout";
+import DashboardLayout from "./Components/DashboardLayout";
+import Fleet from "./Pages/Fleet/Fleet";
 
 export default function App() {
   return (
@@ -10,6 +12,9 @@ export default function App() {
         <Route path="/" element={<LoginLayout />}>
           <Route index element={<Login />} />
           <Route path="/registration" element={<Registration />} />
+        </Route>
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route index element={<Fleet />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/Pages/Login/Registration.js
+++ src/Pages/Login/Registration.js
@@ -1,3 +1,34 @@
+import { useState } from "react";
+
 export default function Registration() {
-  return <h1>Registration</h1>;
+  const [username, setUsername] = useState("");
+
+  function eventHandler(event) {
+    setUsername(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    console.log(username);
+  }
+
+  return (
+    <div className="registration">
+      <h1 className="registration--header">Choose a username</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          name="username"
+          value={username}
+          className="registration--form--username"
+          type="text"
+          placeholder="Username"
+          onChange={eventHandler}
+        />
+        <br />
+        <button className="registration--form--button">
+          Create new account
+        </button>
+      </form>
+    </div>
+  );
 }
