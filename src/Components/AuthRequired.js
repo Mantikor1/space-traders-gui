9618eb850810d563b62f80892b299a5d5d63e06e
Mantikor1 <jonas.toens@kabelsurfer.de>
2023-05-30 23:10:42 +0200

    Added Fleet overview

--- src/Components/AuthRequired.js
+++ src/Components/AuthRequired.js
@@ -13,6 +13,7 @@ export default function AuthRequired() {
       method: "GET",
       headers: {
         Accept: "application/json",
+        // Authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiTUFOVElLT1JURVNUNCIsInZlcnNpb24iOiJ2MiIsInJlc2V0X2RhdGUiOiIyMDIzLTA1LTIwIiwiaWF0IjoxNjg1MjIxNjUzLCJzdWIiOiJhZ2VudC10b2tlbiJ9.Yi2wuT2RAGtxQIxFcGM6DgSqOjZJw-5fCmnixcfmWeC0JLHFkqOAgK4fypQFYMXoYV8qDnznUOvHYKkMnqQ7PAu_ud078ER0IiwN1VfWeCc4jLm3zKWOVXehfUqRezHYnTYXJIBYYY7pkNHYIxAOZhlRbqYYT58pMY1mKQSCVdk7C-2RXD2tQYTOIQ9Do4TXe1zHGHd3oZR4aMhfD5xtULKj-tteKl5rj3a-mn3y-3QKItArPi0diYnlqB2FJxSUOTHQ690GdKOJy7rdBPW6RxhxZ_wI-6tZb4rUSQDfXtPMqkUdcXsM-JLBjPvMjUN-rAqDCGtm0AQRVVDqozFz1w`,
         Authorization: `Bearer ${authToken}`,
       },
     };
@@ -35,7 +36,6 @@ export default function AuthRequired() {
 
   // Wait until fetch request finished, then return either the requested content or the login page
   if (isLoggedIn === false) {
-    console.log("return");
     // Return to login page
     return <Navigate to="/" />;
   } else if (isLoggedIn) {
--- src/Pages/Fleet/Fleet.js
+++ src/Pages/Fleet/Fleet.js
@@ -1,8 +1,74 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import AuthContext from "../../Components/AuthContext";
+import GasStation from "../../assets/Gas_Station.svg";
 
 export default function Fleet() {
+  // Import context for token
   const { authToken } = useContext(AuthContext);
-  console.log(authToken);
-  return <h1>Fleet</h1>;
+
+  // Create a state to hold the ships array
+  const [ships, setShips] = useState([]);
+
+  // Get request for a list of all ships owned by the user
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${authToken}`,
+      },
+    };
+
+    const request = async () => {
+      const response = await fetch(
+        "https://api.spacetraders.io/v2/my/ships/",
+        options
+      );
+
+      if (!response.ok) {
+        console.log(response.status);
+        return;
+      }
+
+      const { data } = await response.json();
+      setShips(data);
+    };
+    request();
+  }, [authToken]);
+
+  // Map over the ship array and create a div for each element
+  const shipElements = ships.map((ship) => {
+    const fuelPercentage = (ship.fuel.current / ship.fuel.capacity) * 100;
+    return (
+      <Link
+        to={ship.symbol}
+        // Remove decoration
+        style={{ textDecoration: "none" }}
+        key={ship.symbol}
+      >
+        <div className="fleet--ship">
+          <p style={{ fontWeight: 700 }}>{ship.symbol}</p>
+          <div className="fleet--ship--details">
+            <p>{ship.nav.waypointSymbol}</p>
+            <p>{ship.nav.status}</p>
+            <img
+              src={GasStation}
+              alt="Gas Station"
+              width="40px"
+              height="40px"
+            />
+            <p>{fuelPercentage}%</p>
+          </div>
+        </div>
+      </Link>
+    );
+  });
+
+  return (
+    <div className="fleet">
+      <h1 className="fleet--header">Fleet</h1>
+      {shipElements}
+    </div>
+  );
 }
