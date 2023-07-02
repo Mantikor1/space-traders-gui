0464e6ab64c7ade913107b9b959452104c1fbc2f
Jonas Toens <jonas.toens@kabelsurfer.de>
2023-07-02 18:25:20 +0200

    StatusNavbar highlighting fixed Gap between ships in fleet added Readme added

--- src/Components/ShipStatusNavbar.js
+++ src/Components/ShipStatusNavbar.js
@@ -4,101 +4,102 @@ import { useContext, useEffect, useState } from "react";
 import AuthContext from "./AuthContext";
 
 export default function ShipStatusNavbar() {
-  const { authToken } = useContext(AuthContext);
+	const { authToken } = useContext(AuthContext);
 
-  // Get ship id from url parameters
-  const params = useParams();
-  const [shipDetails, setShipDetails] = useState({
-    symbol: "",
-    frame: {
-      name: "",
-    },
-    nav: {
-      waypointSymbol: "",
-      status: "",
-      flightMode: "",
-    },
-    fuel: {
-      current: 0,
-      capacity: 0,
-    },
-  });
+	// Get ship id from url parameters
+	const params = useParams();
+	const [shipDetails, setShipDetails] = useState({
+		symbol: "",
+		frame: {
+			name: "",
+		},
+		nav: {
+			waypointSymbol: "",
+			status: "",
+			flightMode: "",
+		},
+		fuel: {
+			current: 0,
+			capacity: 0,
+		},
+	});
 
-  useEffect(() => {
-    const request = async () => {
-      const options = {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          Authorization: `Bearer ${authToken}`,
-        },
-      };
-      const response = await fetch(
-        `https://api.spacetraders.io/v2/my/ships/${params.id}`,
-        options
-      );
+	useEffect(() => {
+		const request = async () => {
+			const options = {
+				method: "GET",
+				headers: {
+					Accept: "application/json",
+					Authorization: `Bearer ${authToken}`,
+				},
+			};
+			const response = await fetch(
+				`https://api.spacetraders.io/v2/my/ships/${params.id}`,
+				options
+			);
 
-      if (!response.ok) {
-        console.log(response.status);
-      }
+			if (!response.ok) {
+				console.log(response.status);
+			}
 
-      const { data } = await response.json();
-      setShipDetails(data);
-    };
-    request();
-  }, [authToken, params.id]);
+			const { data } = await response.json();
+			setShipDetails(data);
+		};
+		request();
+	}, [authToken, params.id]);
 
-  return (
-    <div className="shipStatusNavbar">
-      <div className="shipStatusNavbar--back--layout">
-        <NavLink to=".." style={{ textDecoration: "none" }}>
-          <div className="shipStatusNavbar--back">
-            <img src={ArrowBack} alt="Arrow Back" width="20px" height="20px" />
-            <h3 className="shipStatusNavbar--back--text">Back to ships</h3>
-          </div>
-        </NavLink>
-      </div>
-      <div className="shipStatusNavbar--shipname">
-        <h1 className="shipStatusNavbar--shipname--header">
-          {shipDetails.symbol}
-        </h1>
-        <h2 className="shipStatusNavbar--shipname--frame">
-          {shipDetails.frame.name}
-        </h2>
-      </div>
-      <div className="shipStatusNavbar--nav">
-        <NavLink
-          to="."
-          className={({ isActive }) =>
-            isActive
-              ? "shipStatusNavbar--nav--link--active"
-              : "shipStatusNavbar--nav--link"
-          }
-        >
-          Status
-        </NavLink>
-        <NavLink
-          to="navigation"
-          className={({ isActive }) =>
-            isActive
-              ? "shipStatusNavbar--nav--link--active"
-              : "shipStatusNavbar--nav--link"
-          }
-        >
-          Navigation
-        </NavLink>
-        <NavLink
-          to="cargo"
-          className={({ isActive }) =>
-            isActive
-              ? "shipStatusNavbar--nav--link--active"
-              : "shipStatusNavbar--nav--link"
-          }
-        >
-          Cargo
-        </NavLink>
-      </div>
-      <Outlet context={shipDetails} />
-    </div>
-  );
+	return (
+		<div className="shipStatusNavbar">
+			<div className="shipStatusNavbar--back--layout">
+				<NavLink to=".." style={{ textDecoration: "none" }}>
+					<div className="shipStatusNavbar--back">
+						<img src={ArrowBack} alt="Arrow Back" width="20px" height="20px" />
+						<h3 className="shipStatusNavbar--back--text">Back to ships</h3>
+					</div>
+				</NavLink>
+			</div>
+			<div className="shipStatusNavbar--shipname">
+				<h1 className="shipStatusNavbar--shipname--header">
+					{shipDetails.symbol}
+				</h1>
+				<h2 className="shipStatusNavbar--shipname--frame">
+					{shipDetails.frame.name}
+				</h2>
+			</div>
+			<div className="shipStatusNavbar--nav">
+				<NavLink
+					end
+					to="."
+					className={({ isActive }) =>
+						isActive
+							? "shipStatusNavbar--nav--link--active"
+							: "shipStatusNavbar--nav--link"
+					}
+				>
+					Status
+				</NavLink>
+				<NavLink
+					to="navigation"
+					className={({ isActive }) =>
+						isActive
+							? "shipStatusNavbar--nav--link--active"
+							: "shipStatusNavbar--nav--link"
+					}
+				>
+					Navigation
+				</NavLink>
+				<NavLink
+					to="cargo"
+					className={({ isActive }) =>
+						isActive
+							? "shipStatusNavbar--nav--link--active"
+							: "shipStatusNavbar--nav--link"
+					}
+				>
+					Cargo
+				</NavLink>
+			</div>
+			<Outlet context={shipDetails} />
+		</div>
+	);
 }
--- src/Pages/Fleet/Fleet.js
+++ src/Pages/Fleet/Fleet.js
@@ -4,71 +4,73 @@ import AuthContext from "../../Components/AuthContext";
 import GasStation from "../../assets/Gas_Station.svg";
 
 export default function Fleet() {
-  // Import context for token
-  const { authToken } = useContext(AuthContext);
+	// Import context for token
+	const { authToken } = useContext(AuthContext);
 
-  // Create a state to hold the ships array
-  const [ships, setShips] = useState([]);
+	// Create a state to hold the ships array
+	const [ships, setShips] = useState([]);
 
-  // Get request for a list of all ships owned by the user
-  useEffect(() => {
-    const options = {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: `Bearer ${authToken}`,
-      },
-    };
+	// Get request for a list of all ships owned by the user
+	useEffect(() => {
+		const options = {
+			method: "GET",
+			headers: {
+				Accept: "application/json",
+				Authorization: `Bearer ${authToken}`,
+			},
+		};
 
-    const request = async () => {
-      const response = await fetch(
-        "https://api.spacetraders.io/v2/my/ships/",
-        options
-      );
+		const request = async () => {
+			const response = await fetch(
+				"https://api.spacetraders.io/v2/my/ships/",
+				options
+			);
 
-      if (!response.ok) {
-        console.log(response.status);
-        return;
-      }
+			if (!response.ok) {
+				console.log(response.status);
+				return;
+			}
 
-      const { data } = await response.json();
-      setShips(data);
-    };
-    request();
-  }, [authToken]);
+			const { data } = await response.json();
+			setShips(data);
+		};
+		request();
+	}, [authToken]);
 
-  // Map over the ship array and create a div for each element
-  const shipElements = ships.map((ship) => {
-    const fuelPercentage = (ship.fuel.current / ship.fuel.capacity) * 100;
-    return (
-      <Link
-        to={ship.symbol}
-        // Remove decoration
-        style={{ textDecoration: "none" }}
-        key={ship.symbol}
-      >
-        <div className="fleet--ship">
-          <p style={{ fontWeight: 700 }}>{ship.symbol}</p>
-          <div className="fleet--ship--details">
-            <p>{ship.nav.waypointSymbol}</p>
-            <p>{ship.nav.status}</p>
-            <img
-              src={GasStation}
-              alt="Gas Station"
-              width="40px"
-              height="40px"
-            />
-            <p>{fuelPercentage}%</p>
-          </div>
-        </div>
-      </Link>
-    );
-  });
+	console.log(ships);
 
-  return (
-    <div className="fleet">
-      <h1 className="fleet--header">Fleet</h1>
-      {shipElements}
-    </div>
-  );
+	// Map over the ship array and create a div for each element
+	const shipElements = ships.map((ship) => {
+		const fuelPercentage = (ship.fuel.current / ship.fuel.capacity) * 100;
+		return (
+			<Link
+				to={ship.symbol}
+				// Remove decoration
+				style={{ textDecoration: "none" }}
+				key={ship.symbol}
+			>
+				<div className="fleet--ship">
+					<p style={{ fontWeight: 700 }}>{ship.symbol}</p>
+					<div className="fleet--ship--details">
+						<p>{ship.nav.waypointSymbol}</p>
+						<p>{ship.nav.status}</p>
+						<img
+							src={GasStation}
+							alt="Gas Station"
+							width="40px"
+							height="40px"
+						/>
+						<p>{fuelPercentage}%</p>
+					</div>
+				</div>
+			</Link>
+		);
+	});
+
+	return (
+		<div className="fleet">
+			<h1 className="fleet--header">Fleet</h1>
+			{shipElements}
+		</div>
+	);
 }
